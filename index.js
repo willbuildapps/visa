8bcbdd96c38405e54c30b685b57c917b18661dad
Luiz Castro <luizf.dcastro@gmail.com>
2023-03-05 11:08:47 -0300

    update

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const cors = require('cors')
 const helmet = require('helmet')
 const dotenv = require('dotenv')
 
+dotenv.config({ path: './config.env' })
+
 app.use(helmet())
 app.use(cors())
 app.use(express.json({ limit: 200 }))
@@ -15,5 +17,5 @@ app.use('/v1/reschedule', rescheduleRoutes)
 const port = 8000
 
 app.listen(process.env.PORT || port, () =>
-    console.log(`Running on port ${port}`)
+    console.log(`Running on port ${process.env.PORT || port}`)
 )
